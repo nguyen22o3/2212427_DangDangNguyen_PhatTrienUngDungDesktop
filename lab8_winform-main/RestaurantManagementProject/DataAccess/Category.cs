cda4a622d0be5a2df29dcd142ca4a65abdc51a0e
nguyen22o3 <133405406+nguyen22o3@users.noreply.github.com>
2024-11-12 10:36:55 +0700

    Add files via upload

---
+++ lab8_winform-main/RestaurantManagementProject/DataAccess/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Lớp ánh xạ bảng Category
+    /// </summary>
+    public class Category
+    {
+        // ID của bảng, tự tăng trong CSDL
+        public int ID { get; set; }
+        // Tên của loại thức ăn
+        public string Name { get; set; }
+        // Kiểu: 0 là đố uống; 1 là thức ăn...
+        public int Type { get; set; }
+    }
+    /// <summary>
+    /// Lớp ánh xạ bảng Food
+    /// </summary>
+    /// 
+
+}
+
+
+
